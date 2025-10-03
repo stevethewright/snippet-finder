ae1abf0fd99e85c1d47322852b8fd8ea8a6b294c
stevethewright <37734451+stevethewright@users.noreply.github.com>
2025-10-03 22:52:15 +0930

    Basic implementation using gemini

--- src/snippet-finder/backend.py
+++ src/snippet-finder/backend.py
@@ -1,7 +1,23 @@
 from pathlib import Path
 import faster_whisper
+from google import genai
 
 def generate_transcript(file: Path):
     model = faster_whisper.WhisperModel("base")
     segments, info = model.transcribe(str(file))
     return segments, info
+
+def generate_key_points(segments):
+    full_transcript = ""
+    for segment in segments:
+        full_transcript += f'{{ "start": "{segment.start}", "end": "{segment.end}", "text": "{segment.text}" }}\n'
+    input=f"You are a helpful assistant and will determine 3 - 5 key points from this audio transcript that could be used for social media. The data includes timestamps. Please include the timestamps of the best audio snippets in your response. Here is the transcript: {full_transcript}"
+    client = genai.Client()
+
+    # TODO: Pass in API Key.
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=input
+    )
+
+    # Extract and return the response from the API
+    return response.text
